7a5b20804455f129f792461a703a1ad0c9c76a09
Ann388 <funnyann2017@yandex.ru>
2025-06-05 23:37:24 +0400

    Запрос пути к файлу

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,40 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] agrs) {
+        Scanner scanner = new Scanner(System.in);
+        int x = 0;
+
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            if (!fileExists) {
+                System.out.println("Файл не найден по этому пути.");
+                continue;
+            }
+
+            boolean isDirectory = file.isDirectory();
+            if (!isDirectory) {
+                System.out.println("Указанный путь ведет к папке, а не к файлу.");
+                continue;
+            }
+
+            System.out.println("Путь указан верно.");
+            x++;
+            System.out.println("Это файл номер " + x);
+        }
+    }
+}
+
+
+
+
+
+/*
         System.out.println("Введите первое число: ");
         int firstNumber = new Scanner(System.in).nextInt();
         System.out.println("Введите второе число: ");
@@ -14,5 +47,4 @@ public class Main {
         System.out.println("Произведение: " + mult);
         double quotient = (double) firstNumber / secondNumber;
         System.out.println("Частное: " + quotient);
-    }
-}
+ */
